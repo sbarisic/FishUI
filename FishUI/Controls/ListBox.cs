43c663a13582800f3388cd8bed8b98c8aee11660
Saša Barišić <sbarisic1997@gmail.com>
2025-12-12 15:48:26 +0100

    Scrollable control and list box classes

---
+++ FishUI/Controls/ListBox.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishUI.Controls
+{
+	public class ListBox : Control
+	{
+	}
+}
