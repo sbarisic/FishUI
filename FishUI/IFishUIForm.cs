69bf8d9805e6a63cd5e13cccb9bb7439c4d5e2d8
Saša Barišić <sbarisic@vub.hr>
2026-01-28 23:40:22 +0100

    IFishUIForm

---
+++ FishUI/IFishUIForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace FishUI
+{
+	// TODO: Base interface for .Designer.cs files for FishUI forms
+	public interface IFishUIForm
+	{
+		// Create FishUI and theme here
+		public void Init(FishUISettings UISettings, IFishUIGfx Gfx, IFishUIInput Input, IFishUIEvents Events);
+
+		// Load controls and add them to FUI here
+		public void LoadControls(FishUI FUI);
+
+		// Called after controls are loaded
+		public void OnLoaded();
+	}
+}
